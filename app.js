ce13cbdd22995e93971a1df89a691da99a80a788
Dominik Ubuntu <119432963+ImBoB99@users.noreply.github.com>
2025-05-19 14:36:14 +0200

    added message display to ejs template

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const indexRouter = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
 
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use("/", indexRouter);
-app.use("/new", newRouter)
+app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
