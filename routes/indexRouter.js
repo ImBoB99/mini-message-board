8111ce70167f65f4fe969eb090659c88afe17e3d
Dominik Ubuntu <119432963+ImBoB99@users.noreply.github.com>
2025-05-19 15:07:48 +0200

    added form to add new messages, and dynamic ejs pages that link to each.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,21 +1,9 @@
 const { Router } = require("express");
 const indexRouter = Router();
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+const messages = require("../messages")
 
 indexRouter.get("/", (req, res) => {
-  res.render("index", { messages: messages });
+  res.render("index", { messages });
 });
 
 module.exports = indexRouter;
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,8 +1,12 @@
-const { Router } = require("express")
+const { Router } = require("express");
 const newRouter = Router();
+const messages = require("../messages");
 
-newRouter.get("/", (req, res) => {
-  res.send("New page router setup")
-})
+newRouter.post("/", (req, res) => {
+  const { name, text} = req.body;
+  const newId = messages.length;
+  messages.push({ id: newId, text: text, user: name, added: new Date()});
+  res.redirect("/");
+});
 
 module.exports = newRouter;
