b2b298c270ef53c465b8e859d81826fe0dae5a16
Dominik Ubuntu <119432963+ImBoB99@users.noreply.github.com>
2025-05-19 14:24:26 +0200

    added a /new page router

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const indexRouter = require("./routes/indexRouter")
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
 
-app.use("/", indexRouter)
+app.use("/", indexRouter);
+app.use("/new", newRouter)
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
